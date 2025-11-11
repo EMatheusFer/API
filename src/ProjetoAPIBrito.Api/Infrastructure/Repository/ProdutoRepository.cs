b7a95bd64a54ba7f7b1329c5fe00272856e4b199
EMatheusFer <ematheusfer@outlook.com>
2025-11-11 18:40:48 -0300

    🚧 Criando Estrutura de Produto

---
+++ src/ProjetoAPIBrito.Api/Infrastructure/Repository/ProdutoRepository.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using Microsoft.EntityFrameworkCore;
+using ProjetoAPIBrito.Api.Data;
+using ProjetoAPIBrito.Api.Domain.Models;
+
+namespace ProjetoAPIBrito.Api.Infrastructure.Repository
+{
+    public class ProdutoRepository(AppDbContext context)
+    {
+        private readonly AppDbContext _context = context;
+
+        public async Task<Produto> Adicionar(Produto produto)
+        {
+            produto.Ativo = true;
+            produto.DataInativacao = null;
+
+            await _context.Produtos.AddAsync(produto);
+            await _context.SaveChangesAsync();
+
+            return produto;
+        }
+
+        public async Task<Produto> Atualizar(Produto produto)
+        {
+            _context.Produtos.Update(produto);
+            await _context.SaveChangesAsync();
+
+            return produto;
+        }
+
+        public async Task Inativar(Produto produto)
+        {
+            produto.Ativo = false;
+            produto.DataInativacao = DateTime.UtcNow;
+
+            _context.Produtos.Update(produto);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Reativar(Produto produto)
+        {
+            produto.Ativo = true;
+            produto.DataInativacao = null;
+
+            _context.Produtos.Update(produto);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Produto>> ObterTodos(Produto produto)
+        {
+            return await _context.Produtos.ToListAsync();
+        }
+        public async Task<Produto?> ObterPorId(Produto produto)
+        {
+            return await _context.Produtos.FirstOrDefaultAsync(p => produto.Id.Equals(produto.Id));
+        }
+    }
+}
---
+++ src/ProjetoAPIBrito.Api/Migrations/20251111204002_CriarEntidadeProduto.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ProjetoAPIBrito.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriarEntidadeProduto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Descricao = table.Column<string>(type: "character varying(300)", maxLength: 300, nullable: false),
+                    Preco = table.Column<decimal>(type: "numeric(18,2)", nullable: false),
+                    Ativo = table.Column<bool>(type: "boolean", nullable: false),
+                    DataInativacao = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+        }
+    }
+}
